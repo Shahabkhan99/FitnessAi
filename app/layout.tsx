c82d43b51d6956b4f13ecbaa50ac73b2f05ac406
Rahil <rahiltanna16@gmail.com>
2025-03-24 22:02:31 +0530

    Api fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import  './globals.css'
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +33,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <SpeedInsights/>
         <Analytics />
       </body>
     </html>
